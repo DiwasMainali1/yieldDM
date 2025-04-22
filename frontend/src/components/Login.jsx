567060e4d7d67add1d3f0f59b51c45d24afd3371
Diwas Mainali <diwas.sok@gmail.com>
2025-04-23 09:55:47 +1000

    First commit, setting up

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+const Login = () => {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Login
+            </button>
+        </div>
+    )
+}
+
+export default Login;
